ada0b6e6047519aba30d5d5ce15c00c43e08862d
BigRich727 <102982967+BigRich727@users.noreply.github.com>
2022-04-05 12:54:21 -0400

    Delete rich.py

--- rich.py
+++
@@ -1,7 +0,0 @@
-
-first_number = int(input("What is the first number you want to add?"))
-second_number = int(input("What is the second number you want to add"))
-sum = first_number + second_number
-print('the sum of the two numbers is',sum)        
-print("You are so silly. That is not an integer...")
-
